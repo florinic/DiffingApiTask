ed3be3d2fc00483eff06c504b46d6e7064ca90eb
FDN <florin_nicolae@yahoo.com>
2022-05-20 09:22:59 -0400

    added a test

--- TheUnitTestProject/TestEndpointController.cs
+++ TheUnitTestProject/TestEndpointController.cs
@@ -28,39 +28,62 @@ namespace TheUnitTestProject
             return target;
         }
 
-        [TestMethod]
-        public void SameBodies()
+        [DataTestMethod]
+        [DataRow("AAAAAA==", "AAAAAA==", "{ diffResultType = Equals }")]
+        [DataRow("AAAAAA==", "AQABAQ==", "{ diffResultType = ContentDoNotMatch, diffs = [{ \"offset\":0, \"length\":1},{ \"offset\":2, \"length\":2}] }")]
+        public void MultipleBodies(string left,string right,string diffResult)
         {
-            const int index = 1;
+            var index = new Random().Next(1000); 
 
             var target = GetTarget();
-            var result = target.DiffSide(index, "left", body: new EndpointController.BodyContent { data = "AAAAAA==" });
+
+            var result = target.DiffSide(index, "left", body: new EndpointController.BodyContent { data = left });
+            Debug.WriteLine("diff1:[" + GetProperty((result as JsonResult).Data, "diffResultType") + "]"); // = []
             Assert.AreEqual(target.HttpContext.Response.StatusCode, 201);
 
-            result = target.DiffSide(index, "right", body: new EndpointController.BodyContent { data = "AAAAAA==" });
+            result = target.DiffSide(index, "right", body: new EndpointController.BodyContent { data = right });
+            Debug.WriteLine("diff2:[" + GetProperty((result as JsonResult).Data, "diffResultType") + "]"); // = []
             Assert.AreEqual(target.HttpContext.Response.StatusCode, 201);
 
             result = target.Diff(index);
-            Assert.AreEqual((result as JsonResult).Data.ToString(), "{ diffResultType = Equals }");
+            Debug.WriteLine("diff3:[" + GetProperty((result as JsonResult).Data, "diffResultType") + "]"); // = []
+            Debug.WriteLine("3rd result.Data.diffs:[" + GetProperty((result as JsonResult).Data, "diffs") + "]"); // = []
+            Assert.AreEqual(diffResult, (result as JsonResult).Data.ToString());
         }
 
-        [TestMethod]
-        public void DiferentBodies()
+        [DataTestMethod]
+        [DataRow(false)]
+        [DataRow(true)]
+        public void IntermingledIndexes(bool sameInstance)
         {
-            const int index = 1;
+            var rnd = new Random();
+            var index1 = rnd.Next(1000);
+            var index2 = rnd.Next(1000);
+            if (index1 == index2) index2++;
 
-            var target = GetTarget();
-            var result = target.DiffSide(index, "left", body: new EndpointController.BodyContent { data = "AAAAAA==" });
-            Assert.AreEqual(target.HttpContext.Response.StatusCode, 201);
+            var target1 = GetTarget();
+            var target2 = sameInstance ? target1 : GetTarget();
 
-            result = target.DiffSide(index, "right", body: new EndpointController.BodyContent { data = "AQABAQ==" });
-            Assert.AreEqual(target.HttpContext.Response.StatusCode, 201);
+            var result1 = target1.DiffSide(index1, "left", body: new EndpointController.BodyContent { data = "AAAAAA==" });
+            Assert.AreEqual(target1.HttpContext.Response.StatusCode, 201);
 
-            result = target.Diff(index);
-            Debug.WriteLine((result as JsonResult).Data.ToString());
-            Assert.AreEqual("{ diffResultType = ContentDoNotMatch, diffs = [{ \"offset\":0, \"length\":1},{ \"offset\":2, \"length\":2}] }",
-                (result as JsonResult).Data.ToString());
-            //"{ diffResultType = { \"diffResultType\": \"ContentDoNotMatch\", \"diffs\": [{ \"offset\":1, \"length\":1,{ \"offset\":3, \"length\":1,{ \"offset\":5, \"length\":1]} }");
+            var result2 = target2.DiffSide(index2, "left", body: new EndpointController.BodyContent { data = "AAAAAA==" });
+            Assert.AreEqual(target2.HttpContext.Response.StatusCode, 201);
+
+            result1 = target1.DiffSide(index1, "right", body: new EndpointController.BodyContent { data = "AAAAAA==" });
+            Assert.AreEqual(target1.HttpContext.Response.StatusCode, 201);
+
+            result2 = target2.DiffSide(index2, "right", body: new EndpointController.BodyContent { data = "AQABAQ==" });
+            Assert.AreEqual(target2.HttpContext.Response.StatusCode, 201);
+
+            result1 = target1.Diff(index1);
+            Assert.AreEqual("{ diffResultType = Equals }", (result1 as JsonResult).Data.ToString());
+
+            result2 = target2.Diff(index2);
+            Assert.AreEqual("{ diffResultType = ContentDoNotMatch, diffs = [{ \"offset\":0, \"length\":1},{ \"offset\":2, \"length\":2}] }", (result2 as JsonResult).Data.ToString());
         }
+
+        private static string GetProperty(object target, string propertyName) =>
+            target?.GetType().GetProperty(propertyName)?.GetValue(target)?.ToString();        
     }
 }
